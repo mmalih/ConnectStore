d9f2d4e6fa13a20c1b8e54f9bff140e693439ae7
Maeva <maeva.malih@gmail.com>
2017-07-11 14:46:42 +0200

    interface connexion

---
+++ MenuConnectStore/HomeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HomeViewController.swift
+//  MenuConnectStore
+//
+//  Created by Maeva Malih on 11/07/2017.
+//  Copyright © 2017 Maeva. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var Tconnect: UILabel!
+    
+    @IBAction func Choice(_ sender: Any) {
+    }
+
+    @IBAction func log(_ sender: Any) {
+    }
+    
+    
+    @IBAction func pswd(_ sender: Any) {
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
